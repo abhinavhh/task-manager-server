ea0354442b543d85f7352335ac178c5b46dd7118
Abhinav <abhinavponnu964531@gmail.com>
2025-09-22 14:58:18 +0530

    created user enitity

--- src/main/java/com/example/taskManagement/Configs/SecurityConfig.java
+++ src/main/java/com/example/taskManagement/Configs/SecurityConfig.java
@@ -1,4 +1,5 @@
 package com.example.taskManagement.Configs;
 
 public class SecurityConfig {
+
 }
--- src/main/java/com/example/taskManagement/Entities/User.java
+++ src/main/java/com/example/taskManagement/Entities/User.java
@@ -1,4 +1,21 @@
 package com.example.taskManagement.Entities;
 
+import com.example.taskManagement.Enums.Role;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String email;
+
+    private String password;
+
+    @Enumerated(value = EnumType.STRING)
+    private Role role;
 }
