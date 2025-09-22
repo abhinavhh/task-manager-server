8139988d13c78b68abbadaf5a4d47733594acbc9
Abhinav <abhinavponnu964531@gmail.com>
2025-09-22 22:24:09 +0530

    Created register in AuthController

--- src/main/java/com/example/taskManagement/Controllers/AuthController.java
+++ src/main/java/com/example/taskManagement/Controllers/AuthController.java
@@ -1,9 +1,25 @@
 package com.example.taskManagement.Controllers;
 
+import com.example.taskManagement.Dtos.RegisterRequest;
+import com.example.taskManagement.Services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
 public class AuthController {
+    private UserService userService;
+
+    @PostMapping()
+    public ResponseEntity<?> register(@RequestBody RegisterRequest request){
+        String response = userService.register(request);
+        return ResponseEntity.ok(response);
+    }
+
 }
