50c8a3189f74d4fdd2f2f2a5aff8031d13581286
Abhinav <abhinavponnu964531@gmail.com>
2025-09-22 15:23:38 +0530

    Created task entity

--- src/main/java/com/example/taskManagement/Entities/Task.java
+++ src/main/java/com/example/taskManagement/Entities/Task.java
@@ -1,4 +1,23 @@
 package com.example.taskManagement.Entities;
 
+import com.example.taskManagement.Enums.TaskPriority;
+import com.example.taskManagement.Enums.TaskStatus;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tasks")
 public class Task {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private TaskPriority priority;
+
+    @Enumerated(EnumType.STRING)
+    private TaskStatus status;
 }
